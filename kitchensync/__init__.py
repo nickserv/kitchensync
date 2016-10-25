a571988e8f9287534ef132ffae04e4793b90fd84
Nick McCurdy <thenickperson@gmail.com>
2016-10-25 18:29:24 -0400

    Refactor into functions that represent commands with lists

--- kitchensync/__init__.py
+++ kitchensync/__init__.py
@@ -3,17 +3,19 @@ import os
 import sys
 
 
-def command_for_manager(manager):
-    install_command = ['install']
-    if manager == 'npm':
-        install_command = ['install', '--global']
-    return [manager] + install_command
+def install_command(file):
+    manager = os.path.splitext(os.path.basename(file))[0]
+    options = ['--global'] if manager == 'npm' else []
+    packages = open(file).read().split()
+    return [manager, 'install'] + options + packages
+
+
+def install_commands(directory):
+    files = glob.glob(os.path.join(directory, '*.txt'))
+    return map(install_command, files)
 
 
 def main():
     directory = sys.argv[1] if len(sys.argv) == 2 else 'packages'
-    for file in glob.glob(os.path.join(directory, '*.txt')):
-        basename = os.path.basename(file)
-        manager = os.path.splitext(basename)[0]
-        packages = open(file).read().split()
-        print(' '.join(command_for_manager(manager) + packages))
+    install_command_strings = map(' '.join, install_commands(directory))
+    print('\n'.join(install_command_strings))
