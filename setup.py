6124089236c2d348ce31c15b3ead50b75fd027a9
Nick McCurdy <thenickperson@gmail.com>
2016-10-24 22:28:33 -0400

    Use extras_require instead of requirements.txt

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ import setuptools
 setuptools.setup(
     name='kitchensync',
     packages=['kitchensync'],
+    extras_require={'test': 'cram'},
     entry_points={'console_scripts': 'kitchen=kitchensync:main'}
 )
